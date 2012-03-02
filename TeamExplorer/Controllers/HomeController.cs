b46d8e428484d9e1fb74b8ff8effe8322be89372
Joel Sannerstedt <joel.sannerstedt@gmail.com>
2012-03-02 16:39:38 +0100

    create session set default charter to active

--- TeamExplorer/Controllers/HomeController.cs
+++ TeamExplorer/Controllers/HomeController.cs
@@ -15,22 +15,23 @@ namespace TeamExplorer.Controllers
         public ActionResult Index()
         {
             // Create the first session if it doensn't exists 
-            //var session = DocumentSession.Load<Session>(1);
-            //if (session == null)
-            //{
-            //    DocumentSession.Store(new Session { Date = DateTime.Now, Id = 1337});
-            //    DocumentSession.SaveChanges();
-            //}
-            //var session = DocumentSession.Query<Session>().FirstOrDefault(x => x.Id == 1337);
-            //// Create the first charter if it doensn't exists 
-            //var charter = DocumentSession.Load<Charter>(1);
-            //if (charter == null)
-            //{
-            //    DocumentSession.Store(new Charter {Title = "Raven charter...", Session = session});
-            //    DocumentSession.SaveChanges();
-            //}
+            var session = DocumentSession.Load<Session>(1);
+            if (session == null)
+            {
+                DocumentSession.Store(new Session { Date = DateTime.Now });
+                DocumentSession.SaveChanges();
+                session = DocumentSession.Query<Session>().FirstOrDefault(x => x.Id == 1);
 
-            // IEnumerable<Charter> charters = DocumentSession.Query<Charter>();
+            }
+            // Create the first charter if it doensn't exists 
+            var charter = DocumentSession.Load<Charter>(1);
+            if (charter == null)
+            {
+                DocumentSession.Store(new Charter { Title = "Raven charter...", Session = session,IsActive = true});
+                DocumentSession.SaveChanges();
+            }
+
+            //IEnumerable<Charter> charters = DocumentSession.Query<Charter>();
             IEnumerable<Charter> charters = Query(new ActiveCharters());
             return View(charters);
         }
