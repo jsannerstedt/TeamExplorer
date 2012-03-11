30dfcdad2b3d9487e90a4a98528589820b4478ab
Örjan Sjöholm <orjansjoholm@gmail.com>
2012-03-11 22:57:06 +0100

    Added a new controller for the chrome application. This will not share the css and use the separate controller above

---
+++ TeamExplorer/Controllers/ChromeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using TeamExplorer.Models;
+using TeamExplorer.Queries;
+
+namespace TeamExplorer.Controllers
+{
+    public class ChromeController : DocumentController
+    {
+        public ActionResult Index()
+        {
+            var charters = Query(new ActiveCharters());
+            return View(charters);
+        }
+
+        public ActionResult Explore(string id)
+        {
+            var charter = DocumentSession.Load<Charter>(id);
+            return View(charter);
+        }
+    }
+}
