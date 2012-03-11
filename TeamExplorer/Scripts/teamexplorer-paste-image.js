c52a8c3da473e465167fd6cde631ff30f9d4e890
Örjan Sjöholm <orjansjoholm@gmail.com>
2012-03-11 21:22:21 +0100

    Changed for to match the chrome application.

--- TeamExplorer/Scripts/teamexplorer-paste-image.js
+++ TeamExplorer/Scripts/teamexplorer-paste-image.js
@@ -1,15 +1,31 @@
-﻿document.onpaste = function (event) {
-    var items = event.clipboardData.items;
-    console.log(JSON.stringify(items));
+﻿var TeamExplorer = {};
 
-    var blog = items[0].getAsFile();
-    var reader = new FileReader();
-    reader.onload = function (loaded) {
-        console.log(loaded.target.result);
-        var foo = $("#issue-image");
-        foo.val(loaded.target.result);
-        debugger;
+TeamExplorer.captureImages = function (selector) {
+    var imageLoaded = function (src) {
+        console.log(src);
+        $("#issue-image").val(src);
+        var previews = $(".previews"); // .find("ul.previews");
+        var imageTag = '<li><img src="' + src + '" /></li>';
+        previews.append(imageTag);
+        var imageField = $('<input type="hidden" name="image" />');
+        imageField.val(src);
+        $("form").append(imageField);
     };
 
-    reader.readAsDataURL(blog);
+    /* This will only work for chrome... */
+    document.onpaste = function (event) {
+        var items = event.clipboardData.items;
+        console.log(JSON.stringify(items));
+
+        var blob = items[0].getAsFile();
+        var reader = new FileReader();
+        reader.onload = function (loaded) {
+            imageLoaded(loaded.target.result);
+        };
+
+        reader.readAsDataURL(blob);
+    };
+
 };
+
+
